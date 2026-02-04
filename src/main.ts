279ec90b7c57f98964f6e6118a20059e9feec76d
Murilo Leandro <muriloleandro188@gmail.com>
2026-02-04 17:17:16 -0300

    chore: Criando main, preload, html

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { app, BrowserWindow } from 'electron';
+const { ipcMain } = require('electron');
+
+
 function createWindow() {
  const win = new BrowserWindow({
  width: 800,
- height: 600
+ height: 600,
+ webPreferences:{
+    preload: './dist/preload.js'
+ }
  });
  win.loadFile('./src/index.html');
 }
