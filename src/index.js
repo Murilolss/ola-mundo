7a9615fcbf77c763db1e9b71a65edad34bdf421f
Murilo Leandro <muriloleandro188@gmail.com>
2026-02-04 14:29:57 -0300

    Adicionando electromon

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import { app, BrowserWindow } from 'electron';
 function createWindow() {
-    const win = new BrowserWindow({
-        // fullscreen: true
-        width: 800,
-        height: 600,
-    });
-    win.maximizable();
-    win.loadFile('./src/index.html');
+ const win = new BrowserWindow({
+ width: 800,
+ height: 600
+ });
+ win.loadFile('./src/index.html');
 }
 app.whenReady().then(createWindow);
